e699892f3d40e5398f45e201bff6884c4a000e9c
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-08-05 01:11:35 +0300

    change test

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -21,7 +21,7 @@ class PostmanEchoTest {
         // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some"))
+                .body("data", equalTo("some data"))
         ;
     }
 }
