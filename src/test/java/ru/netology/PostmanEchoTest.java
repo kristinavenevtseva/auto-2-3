73eade7dbb1c9b38c37009cb2512c3e4031133d3
kristinavenevtseva <63412659+kristinavenevtseva@users.noreply.github.com>
2020-08-05 01:01:36 +0300

    all files added

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+    @Test
+    void shouldPostDemo() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        // Выполняемые действия
+                .when()
+                .post("/post")
+        // Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some"))
+        ;
+    }
+}
